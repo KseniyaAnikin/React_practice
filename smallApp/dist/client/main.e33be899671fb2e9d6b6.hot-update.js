292183241491e2e5b7096be0683ff835a41f6eff
KseniyaAnikin <100955276+KseniyaAnikin@users.noreply.github.com>
2023-02-02 20:21:35 +0300

    add App

---
+++ smallApp/dist/client/main.e33be899671fb2e9d6b6.hot-update.js
@@ -0,0 +1,104 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/@hot-loader/react-dom/cjs/react-dom.development.js":
+false,
+
+/***/ "./node_modules/@hot-loader/react-dom/index.js":
+false,
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./src/shared/header.css":
+false,
+
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+false,
+
+/***/ "./node_modules/fast-levenshtein/levenshtein.js":
+false,
+
+/***/ "./node_modules/hoist-non-react-statics/dist/hoist-non-react-statics.cjs.js":
+false,
+
+/***/ "./node_modules/object-assign/index.js":
+false,
+
+/***/ "./node_modules/prop-types/checkPropTypes.js":
+false,
+
+/***/ "./node_modules/prop-types/factoryWithTypeCheckers.js":
+false,
+
+/***/ "./node_modules/prop-types/index.js":
+false,
+
+/***/ "./node_modules/prop-types/lib/ReactPropTypesSecret.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/dist/react-hot-loader.development.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/dist/react-hot-loader.production.min.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/index.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/root.js":
+false,
+
+/***/ "./node_modules/react-is/cjs/react-is.development.js":
+false,
+
+/***/ "./node_modules/react-is/index.js":
+false,
+
+/***/ "./node_modules/react-lifecycles-compat/react-lifecycles-compat.es.js":
+false,
+
+/***/ "./node_modules/react/cjs/react.development.js":
+false,
+
+/***/ "./node_modules/react/index.js":
+false,
+
+/***/ "./node_modules/scheduler/cjs/scheduler-tracing.development.js":
+false,
+
+/***/ "./node_modules/scheduler/cjs/scheduler.development.js":
+false,
+
+/***/ "./node_modules/scheduler/index.js":
+false,
+
+/***/ "./node_modules/scheduler/tracing.js":
+false,
+
+/***/ "./node_modules/shallowequal/index.js":
+false,
+
+/***/ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/amd-define.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/amd-options.js":
+false,
+
+/***/ "./src/client/index.jsx":
+/*!******************************!*\
+  !*** ./src/client/index.jsx ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (24:22)\\nFile was processed with these loaders:\\n * ./node_modules/ts-loader/index.js\\nYou may need an additional loader to handle the result of these loaders.\\n| var ReactDom = __importStar(require(\\\"react-dom\\\"));\\n| window.addEventListener(\\\"load\\\", function () {\\n>     ReactDom.hydrate( > , document.getElementById('react_root'));\\n| });\\n| \");\n\n//# sourceURL=webpack:///./src/client/index.jsx?");
+
+/***/ }),
+
+/***/ "./src/shared/Header.tsx":
+false,
+
+/***/ "./src/shared/header.css":
+false
+
+})
