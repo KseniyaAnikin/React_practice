77a6bf9f2c26aa031920b789694b68ab48fb5471
KseniyaAnikin <100955276+KseniyaAnikin@users.noreply.github.com>
2023-05-01 21:28:52 +0300

    add formik

--- smallApp/src/shared/Post/CommentForm/CommentForm.tsx
+++ smallApp/src/shared/Post/CommentForm/CommentForm.tsx
@@ -1,14 +1,26 @@
 import React, { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 import styles from './commentform.css';
+import { useFormik } from 'formik';
 
 interface ICommentForm{
   name?: string,
   value: string,
-  onChange: (event: ChangeEvent<HTMLTextAreaElement>)=> void,
-  onSubmit: (event: FormEvent)=> void,
+  // onChange: (event: ChangeEvent<HTMLTextAreaElement>)=> void,
+  // onSubmit: (event: FormEvent)=> void,
 }
 
-export function CommentForm({ name, value, onChange, onSubmit}: ICommentForm) {
+const validate = (values: {commentForm: string}) => {
+  const errors = { commentForm : '' };
+
+  if (!values.commentForm) {
+    errors.commentForm = 'leave a comment, please';
+  } else if (values.commentForm.length < 1) {
+    errors.commentForm = 'Must be more than 1 letter';
+  }
+  return errors;
+};
+
+export function CommentForm({ name }: ICommentForm) {
 
   const [ person, setPerson ] = useState('');
   
@@ -22,10 +34,28 @@ export function CommentForm({ name, value, onChange, onSubmit}: ICommentForm) {
     };
   }, [name]);
 
+  const formik = useFormik({
+    initialValues: {
+      commentForm : '' ,
+    },
+    validate,
+    onSubmit: values => {
+      // alert(JSON.stringify(values, null, 2));
+      console.log('OK', values);
+    },
+  });
+
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
-      <textarea className={styles.input} value={value ? value :`${person} оставьте ваш комментарий`} onChange={onChange} ref={formRef}/>
-      <button type='submit' className={styles.button}>Комментировать</button>
+    <form className={styles.form} onSubmit = {formik.handleSubmit}>
+      <textarea name="commentForm"
+        className={styles.input}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.commentForm}/>
+        { formik.touched.commentForm && formik.errors.commentForm ? (
+         <div>{formik.errors.commentForm}</div>
+       ) : null }
+      <button type="submit" className={styles.button}>Комментировать</button>
     </form>
   );
 }
